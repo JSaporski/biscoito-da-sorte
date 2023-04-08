6717594e7443bd979567eac2e14724892a0e61c1
Joao Guilherme <joaoguilherme2411@gmail.com>
2023-04-08 11:29:44 -0300

    fixed the return of 'undefined' at the cookie phrase

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
-const cookie = document.querySelector('#mainContent .luck img')
-const btnAnotherCookie = document.querySelector(
-  '#mainContent .btn-another-cookie'
-)
+// Elements & Variables
+const phrases = [
+  '“É em meio a dificuldade que se encontra a oportunidade”.',
+  '“O êxito é ir de frustração a frustração sem perder a animação”.',
+  '“Mesmo que algo pareça difícil, nunca desista antes de tentar”.',
+  '“Não é uma vida ruim, é apenas um dia ruim, lembre-se disso”.',
+  '“Que os dias bons se tornem rotina, e os ruins se tornem raros”.',
+  '“Já pensou que você já superou muitas dificuldades até aqui?”.',
+  '“Cada dificuldade ultrapassada te faz mais forte”.',
+  '“Desistir não deve ser considerado, mesmo que as coisas não sejam fáceis”.',
+  '“Conheci o pior das pessoas e o melhor de mim, passando por tempos difíceis”.',
+  '“Mesmo que nem todo dia seja bom, há algo de bom todo dia”.',
+  '“A superação da dificuldade depende apenas de você“.',
+  '“Você terá possibilidade de ganhar, enquanto tiver força para lutar”.',
+  '“Não há outra forma de crescer, senão superar os desafios e os medos. Deixe-os vir”.'
+]
+const sectionLuck = document.querySelector('#mainContent .luck')
+const cookie = sectionLuck.querySelector('.btn-show-phrase')
+const anotherCookie = document.querySelector('#mainContent .btn-another-cookie')
+let phrase = document.querySelector('#mainContent .your-luck p')
+let randomNumberPhrase = Math.round(Math.random() * (phrases.length - 1))
 
-cookie.addEventListener('click', () => {
+// Functions || Callbacks
+function handleShowPhrase(event) {
+  event.preventDefault()
+  phrase.innerText = `${phrases[randomNumberPhrase]}`
+
+  handleHideOrShow()
+}
+
+function handleHideOrShow() {
   document.querySelector('#mainContent .luck').classList.toggle('hide')
   document.querySelector('#mainContent .your-luck').classList.toggle('hide')
-})
+}
 
-btnAnotherCookie.addEventListener('click', () => {
-  window.location.reload()
-})
+function handleResetCookie(event) {
+  event.preventDefault()
+  randomNumberPhrase = Math.round(Math.random() * (phrases.length - 1))
+
+  handleHideOrShow()
+}
+
+function handleResetOnEnter(event) {
+  if (event.key === 'Enter' && sectionLuck.classList.contains('hide')) {
+    randomNumberPhrase = Math.round(Math.random() * (phrases.length - 1))
+    handleHideOrShow()
+  }
+}
+
+// Events
+cookie.addEventListener('click', handleShowPhrase)
+anotherCookie.addEventListener('click', handleResetCookie)
+window.addEventListener('keydown', handleResetOnEnter)
